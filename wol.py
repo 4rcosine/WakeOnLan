950dc8d19c2dc67e1b378b8c9f4b09c6979a00fb
Gian Marco <49276912+Metallium96@users.noreply.github.com>
2021-09-17 12:07:43 +0200

    Add files via upload

---
+++ wol.py
@@ -0,0 +1,24 @@
+import socket
+
+def isWol(dati):
+	correct = False
+	correct = dati[0:12] == 'ffffffffffff'
+	mac = dati[12:24] * 16
+	correct = correct and mac == dati[12:]
+	return correct
+	
+ip = "192.168.1.54"
+port = 8000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = (ip, port)
+s.bind(server_address)
+print("Ctrl+c per chiudere il server")
+print("Server in ascolto")
+while True:
+	data, address = s.recvfrom(4096)
+	isW = isWol(data.hex())
+	if isW:
+		print("WOL")
+		s.sendto(data, ("192.168.1.255", 9))
+		
